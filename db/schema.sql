5f7c8067237ee5575ae0e9afb47128f3ec066643
JacksonAhumada <jacksonahumada@gmail.com>
2022-11-06 15:00:25 -0700

    files created, schema being built

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    departmet_id INT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY (DEPARTMENT_ID)
+    REFERENCES DEPARTMENT(ID)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT PRIMARY KEY,
+    FOREIGN KEY (role_id)
+    REFERENCES ROLE(id)
+    FOREIGN KEY (manager_id)
+    REFERENCES EMPLOYEE(id)
+    ON DELETE SET NULL
+);
